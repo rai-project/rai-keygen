87491fc1c124f375c9c77cb4d767ef505c26ded4
Carl Pearson <carl.w.pearson@gmail.com>
2017-11-06 08:37:41 -0600

    Implement getkey command

--- cmd/getkey.go
+++ cmd/getkey.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/rai-project/auth"
+	"github.com/rai-project/auth/provider"
+	"github.com/spf13/cobra"
+)
 
 var getKeyCmd = &cobra.Command{
 	Use:   "getkey",
@@ -8,14 +14,26 @@ var getKeyCmd = &cobra.Command{
 	Long:  "The config file must be configured with the propper mailing credentials.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		log := log.WithField("cmd", "getkey")
+		// log := log.WithField("cmd", "getkey")
 
-		log.Fatal("Unimplemented")
+		prof, err := provider.New(
+			auth.Email(email),
+		)
+		if err != nil {
+			return err
+		}
+
+		if err := prof.GetByEmail(); err != nil {
+			return err
+		}
+
+		fmt.Print(prof.Info().String())
 
 		return nil
 	},
 }
 
 func init() {
+	getKeyCmd.Flags().StringVarP(&email, "email", "e", "", "The email to get the key for.")
 	RootCmd.AddCommand(getKeyCmd)
 }
