a83561722f88cd0d2dd98e9e5b2f08a9ac013f51
Carl Pearson <pearson@illinois.edu>
2017-11-06 20:12:16 -0600

    Some fixes to delete

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/rai-project/auth"
+	"github.com/rai-project/auth/provider"
+	"github.com/spf13/cobra"
+)
 
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
@@ -8,14 +12,20 @@ var deleteCmd = &cobra.Command{
 	Long:  "The config file must be configured with the propper mailing credentials.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		log := log.WithField("cmd", "delete")
+		prof, err := provider.New(
+			auth.Username(""),
+			auth.Email(email),
+		)
+		if err != nil {
+			return err
+		}
 
-		log.Fatal("Unimplemented")
-
-		return nil
+		err = prof.Delete()
+		return err
 	},
 }
 
 func init() {
+	deleteCmd.Flags().StringVarP(&email, "email", "e", "", "The email for the account to delete.")
 	RootCmd.AddCommand(deleteCmd)
 }
